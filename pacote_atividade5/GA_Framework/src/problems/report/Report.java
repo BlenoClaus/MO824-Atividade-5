76b7921be1a870eb8a79a9f8a8b559e34ad6c86a
Bleno Claus <blenoclaus@gmail.com>
2019-04-24 10:51:46 -0300

    Add ao Report Uniform e SteadyState
    Escolher qual rodar pelas variaveis:
    	runStandard
    	runUniformCrossover
    	runSteadyState

--- pacote_atividade5/GA_Framework/src/problems/report/Report.java
+++ pacote_atividade5/GA_Framework/src/problems/report/Report.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import problems.log.Log;
 import problems.qbfpt.solvers.GA_QBFPT;
+import problems.qbfpt.solvers.GA_QBFPT_SteadyState;
+import problems.qbfpt.solvers.GA_QBFPT_UniformCrossover;
 import solutions.Solution;
 
 public class Report {
@@ -47,8 +49,8 @@ public class Report {
 	
 	public static void main(String[] args) throws InterruptedException {
 		
-		Boolean runStandard = Boolean.TRUE;
-		Boolean runUniformCrossover = Boolean.FALSE;
+		Boolean runStandard = Boolean.FALSE;
+		Boolean runUniformCrossover = Boolean.TRUE;
 		Boolean runSteadyState = Boolean.FALSE;
 		
 		if (runStandard) {
@@ -68,15 +70,35 @@ public class Report {
 		}
 		
 		if (runSteadyState) {
-			/*
-			 *  TODO: SteadyState
-			 */
+			for (int i = 0; i < INSTANCES.length; i++) 
+			for (int j = 0; j < POPULATION_SIZE.length; j++) 
+			for (int t = 0; t < MUTATION_RATE.length; t++) {
+				final int z = i;
+				final int h = j;
+				final int v = t;
+				new Thread() {
+					public void run() {
+						gaSteadyStateJob(z, h, v);
+					};
+				}.start();
+				Thread.sleep(10000);
+			}
 		}
 		
 		if (runUniformCrossover) {
-			/*
-			 *  TODO: UniformCrossover
-			 */
+			for (int i = 0; i < INSTANCES.length; i++) 
+			for (int j = 0; j < POPULATION_SIZE.length; j++) 
+			for (int t = 0; t < MUTATION_RATE.length; t++) {
+				final int z = i;
+				final int h = j;
+				final int v = t;
+				new Thread() {
+					public void run() {
+						gaUniformCrossoverJob(z, h, v);
+					};
+				}.start();
+				Thread.sleep(10000);
+			}
 		}
 		
 	}
@@ -92,6 +114,50 @@ public class Report {
 			Log.getLogger(fileLogName).info("\tMutação taxa		: "+MUTATION_RATE[v]/POPULATION_SIZE[h]);
 			Log.getLogger(fileLogName).info("\tAlgoritmo		: Padrão");
 			GA_QBFPT solver = new GA_QBFPT(INTERATIONS[z], POPULATION_SIZE[h], MUTATION_RATE[v]/POPULATION_SIZE[h], INSTANCES[z]);
+			Solution<Integer> solve = solver.solve();
+			Log.getLogger(fileLogName).info(solver.getReport());
+			Log.getLogger(fileLogName).info("\t\tmaxVal = " + solve);
+			Log.getLogger(fileLogName).info("\n}");
+		} catch (IOException e) {
+			Log.getLogger(fileLogName).info("Erro: "+e.getStackTrace().toString());
+			e.printStackTrace();
+		}
+		System.out.println("Thread: "+Thread.currentThread().getName()+ "end!");
+	}
+	
+	private static void gaUniformCrossoverJob(final int z, final int h, final int v) {
+		final String fileLogName = getLogFileName("uniform-crossove",z,h,v);
+		try {
+			Thread.currentThread().setName(fileLogName);
+			System.out.println("Thread: "+Thread.currentThread().getName()+ " start!");
+			Log.getLogger(fileLogName).info("{\n\tInstancia		: "+INSTANCES[z]);
+			Log.getLogger(fileLogName).info("\tInterações		: "+INTERATIONS[z]);
+			Log.getLogger(fileLogName).info("\tTamanho pop.		: "+POPULATION_SIZE[h]);
+			Log.getLogger(fileLogName).info("\tMutação taxa		: "+MUTATION_RATE[v]/POPULATION_SIZE[h]);
+			Log.getLogger(fileLogName).info("\tAlgoritmo		: UniformCrossove");
+			GA_QBFPT_UniformCrossover solver = new GA_QBFPT_UniformCrossover(INTERATIONS[z], POPULATION_SIZE[h], MUTATION_RATE[v]/POPULATION_SIZE[h], INSTANCES[z]);
+			Solution<Integer> solve = solver.solve();
+			Log.getLogger(fileLogName).info(solver.getReport());
+			Log.getLogger(fileLogName).info("\t\tmaxVal = " + solve);
+			Log.getLogger(fileLogName).info("\n}");
+		} catch (IOException e) {
+			Log.getLogger(fileLogName).info("Erro: "+e.getStackTrace().toString());
+			e.printStackTrace();
+		}
+		System.out.println("Thread: "+Thread.currentThread().getName()+ "end!");
+	}
+	
+	private static void gaSteadyStateJob(final int z, final int h, final int v) {
+		final String fileLogName = getLogFileName("steady-state",z,h,v);
+		try {
+			Thread.currentThread().setName(fileLogName);
+			System.out.println("Thread: "+Thread.currentThread().getName()+ " start!");
+			Log.getLogger(fileLogName).info("{\n\tInstancia		: "+INSTANCES[z]);
+			Log.getLogger(fileLogName).info("\tInterações		: "+INTERATIONS[z]);
+			Log.getLogger(fileLogName).info("\tTamanho pop.		: "+POPULATION_SIZE[h]);
+			Log.getLogger(fileLogName).info("\tMutação taxa		: "+MUTATION_RATE[v]/POPULATION_SIZE[h]);
+			Log.getLogger(fileLogName).info("\tAlgoritmo		: SteadyState");
+			GA_QBFPT_SteadyState solver = new GA_QBFPT_SteadyState(INTERATIONS[z], POPULATION_SIZE[h], MUTATION_RATE[v]/POPULATION_SIZE[h], INSTANCES[z]);
 			Solution<Integer> solve = solver.solve();
 			Log.getLogger(fileLogName).info(solver.getReport());
 			Log.getLogger(fileLogName).info("\t\tmaxVal = " + solve);
